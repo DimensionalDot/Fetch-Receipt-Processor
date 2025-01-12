d311eee46bd94b713998a568a9031cf51124d022
DimensionalDot <dimensionaldot@gmail.com>
2025-01-11 22:22:56 -0500

    feat: points calculation and endpoint handling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use chrono::{NaiveDate, NaiveTime};
+use chrono::{Datelike, NaiveDate, NaiveTime};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::{
     collections::HashMap,
     convert::Infallible,
+    str::FromStr,
     sync::{Arc, Mutex},
 };
 use warp::{http::StatusCode, reply::Reply, Filter};
@@ -20,11 +21,69 @@ struct Reciept {
     total: String,
 }
 
+impl Reciept {
+    fn points(&self) -> usize {
+        let mut points = 0;
+
+        points += self // 1 point per alphanumeric in retailer name
+            .retailer
+            .chars()
+            .filter(|&c| c.is_alphanumeric())
+            .count();
+
+        let total: f32 = self
+            .total
+            .parse()
+            .expect("total should have been checked to be a valid f32");
+
+        if total % 1.0 == 0.0 {
+            points += 50;
+        }
+
+        if total % 0.25 == 0.0 {
+            points += 25;
+        }
+
+        points += self.items.len() / 2 * 5; // integer division
+
+        for item in self.items.iter() {
+            points += item.points();
+        }
+
+        if self.purchase_date.day() % 2 == 1 {
+            points += 6
+        }
+
+        let min_time = NaiveTime::from_str("14:00").unwrap();
+        let max_time = NaiveTime::from_str("16:00").unwrap();
+        if min_time < self.purchase_time && self.purchase_time < max_time {
+            points += 10;
+        }
+
+        return points;
+    }
+}
+
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct Item {
     short_description: String,
     price: String,
+}
+
+impl Item {
+    pub fn points(&self) -> usize {
+        if self.short_description.trim().chars().count() % 3 == 0 {
+            let price: f32 = self
+                .price
+                .parse()
+                .expect("price should have been checked to be a valid f32");
+
+            (price * 0.2).ceil() as usize
+        } else {
+            0
+        }
+    }
 }
 
 #[tokio::main]
@@ -45,10 +104,22 @@ async fn main() {
             } else {
                 StatusCode::INTERNAL_SERVER_ERROR.into_response()
             }
-
         });
 
-    let routes = process;
+    let points = warp::get()
+        .and(warp::path!(usize / "points"))
+        .and(with_reciepts(reciepts.clone()))
+        .map(move |id: usize, reciepts: Arc<Mutex<Reciepts>>| {
+            let reciepts = reciepts.lock().unwrap(); // TODO unwrap
+            if let Some(reciept) = reciepts.get(&id) {
+                let points = reciept.points();
+                warp::reply::json(&json![{ "points": points}]).into_response()
+            } else {
+                StatusCode::NOT_FOUND.into_response()
+            }
+        });
+
+    let routes = warp::path("reciepts").and(process.or(points));
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
